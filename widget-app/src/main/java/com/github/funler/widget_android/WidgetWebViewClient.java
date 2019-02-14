2966a6868668880927025ae3ca12b400b9fcf1bc
Mikhail Chachkouski <doctorrokter@gmail.com>
2019-02-14 07:38:30 +0300

    - cache js chunks, images and restore it from internal storage

--- widget-app/src/main/java/com/github/funler/widget_android/WidgetWebViewClient.java
+++ widget-app/src/main/java/com/github/funler/widget_android/WidgetWebViewClient.java
@@ -2,6 +2,8 @@ package com.github.funler.widget_android;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.util.Log;
 import android.webkit.WebResourceRequest;
@@ -33,33 +35,78 @@ class WidgetWebViewClient extends BridgeWebViewClient {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest req) {
-        return shouldInterceptRequest(view, req.getUrl().toString());
+        if (req.getMethod().equals("GET")) {
+            return shouldInterceptRequest(view, req.getUrl().toString());
+        }
+
+        return null;
     }
 
     @Override
     public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
         WebResourceResponse response = null;
 
-        if (url.toLowerCase().endsWith(".chunk.js")) {
-            Log.d(TAG, url);
+        Log.d(TAG, url);
+        String urlLower = url.toLowerCase();
+
+        if (isCacheable(urlLower)) {
 
             try {
-                String[] urlParts = url.split("/");
+                String[] urlParts = urlLower.split("/");
                 String fileName = urlParts[urlParts.length - 1];
+                String ext = getExt(fileName);
 
                 File file = new File(bridgeWebView.getContext().getFilesDir(), fileName);
                 if (!file.exists()) {
-                    saveTextFile(url, fileName);
+                    if (isImage(ext)) {
+                        Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.PNG;
+                        if (Cacheable.jpg.name().equals(ext)) {
+                            compressFormat = Bitmap.CompressFormat.JPEG;
+                        }
+
+                        saveImageFile(urlLower, fileName, compressFormat);
+                    } else {
+                        saveTextFile(urlLower, fileName);
+                    }
                 }
 
                 Log.d(TAG, "Read file from internal storage " + fileName);
-                response = generateWebResourceResponse("text/javascript", "utf-8", bridgeWebView.getContext().openFileInput(fileName));
+                response = generateWebResourceResponse(Cacheable.valueOf(ext).mimeType(), "utf-8", bridgeWebView.getContext().openFileInput(fileName));
             } catch (IOException e) {
                 Log.e(TAG, e.getMessage());
             }
         }
 
         return response;
+    }
+
+    private boolean isCacheable(String url) {
+        return isInCacheableEnum(url) && notExcluded(url);
+    }
+
+    private boolean isInCacheableEnum(String url) {
+        for (Cacheable c : Cacheable.values()) {
+            if (url.endsWith(c.name())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean notExcluded(String url) {
+        return !url.contains("bee_mobile.js") &&
+                !url.contains("jquery.min.js") &&
+                !url.endsWith("bundle.js");
+    }
+
+    private String getExt(String filename) {
+        return filename.substring(filename.lastIndexOf(".") + 1);
+    }
+
+    private boolean isImage(String ext) {
+        return Cacheable.jpg.name().equals(ext) ||
+                Cacheable.png.name().equals(ext);
     }
 
     private void saveTextFile(String url, String fileName) throws IOException {
@@ -79,6 +126,20 @@ class WidgetWebViewClient extends BridgeWebViewClient {
         fos.close();
     }
 
+    private void saveImageFile(String url, String fileName, Bitmap.CompressFormat compressFormat) throws IOException {
+        Log.d(TAG, "Save file to internal storage " + fileName);
+        FileOutputStream fos = bridgeWebView.getContext().openFileOutput(fileName, Context.MODE_PRIVATE);
+
+        URL urlObj = new URL(url);
+        URLConnection urlConnection = urlObj.openConnection();
+        InputStream is = urlConnection.getInputStream();
+        Bitmap image = BitmapFactory.decodeStream(is);
+        image.compress(compressFormat, 100, fos);
+
+        is.close();
+        fos.close();
+    }
+
     private WebResourceResponse generateWebResourceResponse(String mimeType, String encoding, InputStream inputStream) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             int statusCode = 200;
@@ -89,5 +150,19 @@ class WidgetWebViewClient extends BridgeWebViewClient {
         }
 
         return new WebResourceResponse(mimeType, encoding, inputStream);
+    }
+
+    private enum Cacheable {
+        js("text/javascript"),
+        png("image/png"),
+        jpg("image/jpeg");
+
+        private String mimeType;
+
+        Cacheable(String mimeType) {
+            this.mimeType = mimeType;
+        }
+
+        public String mimeType() { return mimeType; }
     }
 }
