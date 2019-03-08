9b340ba9cf99fbe796fd1d33b300b5841aab13a6
Mikhail Chachkouski <doctorrokter@gmail.com>
2019-03-08 22:51:10 +0300

    - px to dp and dp to px

---
+++ widget-app/src/main/java/com/github/funler/widget_android/WidgetUtil.java
@@ -0,0 +1,13 @@
+package com.github.funler.widget_android;
+
+import android.content.Context;
+
+public class WidgetUtil {
+    public static float dpFromPx(Context context, float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float pxFromDp(Context context, float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+}
